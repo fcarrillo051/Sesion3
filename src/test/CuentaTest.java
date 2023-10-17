a1257352844e75a649a0ad112f638c99ef1bc2b7
Fernando <Fernando@DESKTOP-A7U56BC>
2023-10-17 19:04:26 +0200

    TDD paso 1, crear prueba

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pkg.Cuenta;
+
+class CuentaTest {
+
+	Cuenta cuenta;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta = new Cuenta();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar(3000);
+		assertEquals(3000, cuenta.getSaldo());
+	}
+
+	@Test
+	void testRetirar() {
+		fail("Not yet implemented");
+	}
+}
